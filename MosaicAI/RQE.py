015dfefdfa5f30e5e639b318f6ec908195f5fd2a
eahenle <eahenle@gmail.com>
2025-01-24 22:11:36 -0800

    RQE

--- MosaicAI/RQE.py
+++ MosaicAI/RQE.py
@@ -6,21 +6,29 @@ class RouterQueryEngine(Agent):
     def __init__(self, openai_key_path=_DEFAULTS["OpenAI API key path"], model=_DEFAULTS["OpenAI model"], mem0_key_path=_DEFAULTS["mem0 API key path"]):
         super().__init__(openai_key_path=openai_key_path, model=model, mem0_key_path=mem0_key_path)
         self.agents = {"imdbot": IMDBot(), "fallback": super()}
-        self.system_message["classify_query"] = "Is this a movie-related query?"
+        self.system_message["recommend_tool"] = open("system/recommend_tool.txt", "r").read()
         return
     
     def query(self, usr_query:str) -> str:
         """
         Route the query to the appropriate agent based on the query content.
         """
-        # ask LLM to classify the query
-        is_movie_query = super().query(usr_query, system_message=self.system_message["classify_query"]) == "Yes"
+        # classify the query
+        tool = self.recommend_tool(usr_query)
+        print("## TOOL ##", tool)
         # if about movies, ask IMDBot
-        if is_movie_query:
-            return self.agents["imdbot"].query(usr_query)
+        if tool in self.agents.keys():
+            return self.agents[tool].query(usr_query)
         # otherwise, ask fallback agent
         else:
             return self.agents["fallback"].query(usr_query)
+        
+    def recommend_tool(self, usr_query):
+        """
+        Check what the query is about.
+        """
+        response = super().query(usr_query, self.system_message["recommend_tool"])
+        return response.lower().strip()
 
 if __name__ == "__main__":
     rqe = RouterQueryEngine()
