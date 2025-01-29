7c4fb8b55dffbf0b8c3adba31e5ab9a71dfd9f84
Adrian Henle <eahenle@gmail.com>
2025-01-29 13:44:16 -0800

    CI style bot (#27)
    * black.yml
    * auto-format

--- src/MosaicAI/Agent/RouterQueryEngine/RouterQueryEngine.py
+++ src/MosaicAI/Agent/RouterQueryEngine/RouterQueryEngine.py
@@ -2,14 +2,20 @@ from ..Agent import Agent
 from ..IMDBot.IMDBot import IMDBot
 from ...config import _DEFAULTS
 
+
 class RouterQueryEngine(Agent):
-    def __init__(self, openai_key_path=_DEFAULTS["OpenAI API key path"], model=_DEFAULTS["OpenAI model"], mem0_key_path=_DEFAULTS["mem0 API key path"]):
+    def __init__(
+        self,
+        openai_key_path=_DEFAULTS["OpenAI API key path"],
+        model=_DEFAULTS["OpenAI model"],
+        mem0_key_path=_DEFAULTS["mem0 API key path"],
+    ):
         super().__init__(openai_key_path=openai_key_path, model=model, mem0_key_path=mem0_key_path)
         self.agents = {"imdbot": IMDBot(), "fallback": super()}
         self.system_message["recommend_tool"] = open("system/recommend_tool.txt", "r").read()
         return
-    
-    def query(self, usr_query:str) -> str:
+
+    def query(self, usr_query: str) -> str:
         """
         Route the query to the appropriate agent based on the query content.
         """
@@ -22,7 +28,7 @@ class RouterQueryEngine(Agent):
         # otherwise, ask fallback agent
         else:
             return self.agents["fallback"].query(usr_query)
-        
+
     def recommend_tool(self, usr_query):
         """
         Check what the query is about.
@@ -30,10 +36,11 @@ class RouterQueryEngine(Agent):
         response = super().query(usr_query, self.system_message["recommend_tool"])
         return response.lower().strip()
 
+
 if __name__ == "__main__":
     rqe = RouterQueryEngine()
 
-    def test_query(query:str) -> None:
+    def test_query(query: str) -> None:
         print("## QUERY ##\n", query)
         print("## RESPONSE ##\n", rqe.query(query))
         # print("## MEMORY ##\n", rqe.mem0.search(query)) ## TODO: figure this out
